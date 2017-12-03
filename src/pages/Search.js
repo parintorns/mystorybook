7b648c43e87a6a137ab3d5ea2e0213c4d058d82c
Parintorn Sukhowatanakit <parintorns@gmail.com>
2017-12-03 18:08:52 +0700

    Search Page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,23 +4,91 @@ import PropTypes from 'prop-types'
 import {
   Container,
   Header,
+  Content,
   Title,
   Left,
   Right,
-  Body
+  Body,
+  View,
+  List,
+  ListItem,
+  Icon,
+  Item,
+  Input,
+  Text,
+  Segment,
+  Button
 } from 'native-base'
 
 export class Search extends Component {
   render () {
     return (
       <Container>
-        <Header>
-          <Left />
-          <Body>
-            <Title>Search</Title>
-          </Body>
-          <Right />
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input
+              placeholder="Search"
+            />
+            <Icon
+              name="ios-close-circle-outline"
+            />
+          </Item>
         </Header>
+        <Content>
+          <Segment>
+            <Button>
+              <Text>Recents</Text>
+            </Button>
+            <Button>
+              <Text>People</Text>
+            </Button>
+            <Button>
+              <Text>Keywords</Text>
+            </Button>
+            <Button active>
+              <Text>Places</Text>
+            </Button>
+          </Segment>
+          <List>
+            <ListItem avatar button>
+              <Left>
+                <Icon name='ios-pin-outline' />
+              </Left>
+              <Body>
+                <Text>Result Title</Text>
+                <Text note>result note</Text>
+              </Body>
+              <Right>
+                <Icon name='ios-arrow-forward' />
+              </Right>
+            </ListItem>
+            <ListItem avatar button>
+              <Left>
+                <Icon name='ios-pin-outline' />
+              </Left>
+              <Body>
+                <Text>Result Title</Text>
+                <Text note>result note</Text>
+              </Body>
+              <Right>
+                <Icon name='ios-arrow-forward' />
+              </Right>
+            </ListItem>
+            <ListItem avatar button>
+              <Left>
+                <Icon name='ios-pin-outline' />
+              </Left>
+              <Body>
+                <Text>Result Title</Text>
+                <Text note>result note</Text>
+              </Body>
+              <Right>
+                <Icon name='ios-arrow-forward' />
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
       </Container>
     )
   }
